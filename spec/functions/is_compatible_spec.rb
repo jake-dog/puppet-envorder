6218ff50febc3a207287fe6a15d6c972cbd45c8a
Jake Comer <jake-dog@users.noreply.github.com>
2016-09-18 18:08:40 -0400

    adding more tests

--- spec/functions/is_compatible_spec.rb
+++ spec/functions/is_compatible_spec.rb
@@ -124,6 +124,43 @@ describe 'is_compatible' do
       is_expected.to run.with_params({'tree'=> {'min' => '1.5.0-3', 'max' => '1.6.0-1', 'environment' => 'staging', 'query' => 'ipaddress=\'192.168.136.132\''}}).and_return(true)
     end
     
+    it 'should find incompatible version' do
+      PuppetDB::Connection.any_instance.expects(:resources)
+        .with(['in', 'certname', ['extract', 'certname', ['select-facts', ['and', ['=', 'name', 'environment'], ['=', 'value', 'production']]]]],
+              ["and", ["=", "exported", false], ["=", "type", "Package"], ["=", "title", "tree"]])
+        .returns({
+          "localhost.localdomain" =>
+            [
+              {
+                "title"=>"tree",
+                "certname"=>"localhost.localdomain",
+                "resource"=>"56933d99a4f403b05ef65a36fb3eded367f95a1c",
+                "tags"=>["resource", "package", "class", "package-tree", "tree", "resource_tree", "resource_tree::resource", "default", "node"],
+                "parameters"=>{"ensure"=>"1.5.3-3.el6", "provider"=>"yum", "before"=>"Resource_tree::Placeholder[package-tree]"},
+                "type"=>"Package",
+                "file"=>nil,
+                "exported"=>false,
+                "line"=>nil
+              }
+            ],
+          "puppet.localdomain" =>
+            [
+              {
+                "title"=>"tree",
+                "certname"=>"localhost.localdomain",
+                "resource"=>"56933d99a4f403b05ef65a36fb3eded367f95a1c",
+                "tags"=>["resource", "package", "class", "package-tree", "tree", "resource_tree", "resource_tree::resource", "default", "node"],
+                "parameters"=>{"ensure"=>"1.4.1-1.el6", "provider"=>"yum", "before"=>"Resource_tree::Placeholder[package-tree]"},
+                "type"=>"Package",
+                "file"=>nil,
+                "exported"=>false,
+                "line"=>nil
+              }
+            ] 
+          })
+      is_expected.to run.with_params({'tree'=> {'min' => '1.5.0-3', 'max' => '1.6.0-1'}}).and_return(false)
+    end
+    
     it 'should allow fact query' do
       PuppetDB::Connection.any_instance.expects(:facts)
         .with("puppetversion",
@@ -154,6 +191,17 @@ describe 'is_compatible' do
           )
       is_expected.to run.with_params({'puppet'=> {'min' => '3.6.0', 'max' => '4.0.0', 'fact' => 'puppetversion', 'query' => 'ipaddress=\'192.168.136.132\''}}).and_return(true)
     end
+    
+    it 'should find incompatible fact with fact query' do
+      PuppetDB::Connection.any_instance.expects(:facts)
+        .with("puppetversion",
+              ['in', 'certname', ['extract', 'certname', ['select-facts', ['and', ['=', 'name', 'environment'], ['=', 'value', 'production']]]]])
+        .returns(
+            {"localhost.localdomain"=>{"puppetversion"=>"3.8.7"},
+             "puppet.localdomain"=>{"puppetversion"=>"3.4.3"}}
+          )
+      is_expected.to run.with_params({'puppet'=> {'min' => '3.6.0', 'max' => '4.0.0', 'fact' => 'puppetversion'}}).and_return(false)
+    end
   end
   
   context 'with no environment specified' do
@@ -178,6 +226,82 @@ describe 'is_compatible' do
             ]
           })
       is_expected.to run.with_params({'tree'=> {'min' => '1.5.0-3', 'max' => '1.6.0-1', 'query' => 'ipaddress=\'192.168.136.132\''}}).and_return(true)
+    end
+    
+    it 'should work with no extra params' do
+      PuppetDB::Connection.any_instance.expects(:resources)
+        .with(nil,
+              ["and", ["=", "exported", false], ["=", "type", "Package"], ["=", "title", "tree"]])
+        .returns({
+          "localhost.localdomain" =>
+            [
+              {
+                "title"=>"tree",
+                "certname"=>"localhost.localdomain",
+                "resource"=>"56933d99a4f403b05ef65a36fb3eded367f95a1c",
+                "tags"=>["resource", "package", "class", "package-tree", "tree", "resource_tree", "resource_tree::resource", "default", "node"],
+                "parameters"=>{"ensure"=>"1.5.3-3.el6", "provider"=>"yum", "before"=>"Resource_tree::Placeholder[package-tree]"},
+                "type"=>"Package",
+                "file"=>nil,
+                "exported"=>false,
+                "line"=>nil
+              }
+            ]
+          })
+      is_expected.to run.with_params({'tree'=> {'min' => '1.5.0-3', 'max' => '1.6.0-1'}}).and_return(true)
+    end
+    
+    it 'should allow explicit environment' do
+      PuppetDB::Connection.any_instance.expects(:resources)
+        .with(['in', 'certname', ['extract', 'certname', ['select-facts', ['and', ['=', 'name', 'environment'], ['=', 'value', 'production']]]]],
+              ["and", ["=", "exported", false], ["=", "type", "Package"], ["=", "title", "tree"]])
+        .returns({
+          "localhost.localdomain" =>
+            [
+              {
+                "title"=>"tree",
+                "certname"=>"localhost.localdomain",
+                "resource"=>"56933d99a4f403b05ef65a36fb3eded367f95a1c",
+                "tags"=>["resource", "package", "class", "package-tree", "tree", "resource_tree", "resource_tree::resource", "default", "node"],
+                "parameters"=>{"ensure"=>"1.5.3-3.el6", "provider"=>"yum", "before"=>"Resource_tree::Placeholder[package-tree]"},
+                "type"=>"Package",
+                "file"=>nil,
+                "exported"=>false,
+                "line"=>nil
+              }
+            ]
+          })
+      is_expected.to run.with_params({'tree'=> {'min' => '1.5.0-3', 'max' => '1.6.0-1', 'environment' => 'production'}}).and_return(true)
+    end
+    
+    it 'should allow fact query' do
+      PuppetDB::Connection.any_instance.expects(:facts)
+        .with("puppetversion",
+              nil)
+        .returns(
+            {"localhost.localdomain"=>{"puppetversion"=>"3.8.7"}}
+          )
+      is_expected.to run.with_params({'puppet'=> {'min' => '3.6.0', 'max' => '4.0.0', 'fact' => 'puppetversion'}}).and_return(true)
+    end
+    
+    it 'should allow fact query and node query' do
+      PuppetDB::Connection.any_instance.expects(:facts)
+        .with("puppetversion",
+              ["in", "certname", ["extract", "certname", ["select-facts", ["and", ["=", "name", "ipaddress"], ["=", "value", "192.168.136.132"]]]]])
+        .returns(
+            {"localhost.localdomain"=>{"puppetversion"=>"3.8.7"}}
+          )
+      is_expected.to run.with_params({'puppet'=> {'min' => '3.6.0', 'max' => '4.0.0', 'fact' => 'puppetversion', 'query' => 'ipaddress=\'192.168.136.132\''}}).and_return(true)
+    end
+    
+    it 'should allow explicit environment and fact query' do
+      PuppetDB::Connection.any_instance.expects(:facts)
+        .with("puppetversion",
+              ['in', 'certname', ['extract', 'certname', ['select-facts', ['and', ['=', 'name', 'environment'], ['=', 'value', 'production']]]]])
+        .returns(
+            {"localhost.localdomain"=>{"puppetversion"=>"3.8.7"}}
+          )
+      is_expected.to run.with_params({'puppet'=> {'min' => '3.6.0', 'max' => '4.0.0', 'fact' => 'puppetversion', 'environment' => 'production'}}).and_return(true)
     end
   end
 end
